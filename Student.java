9c6f15bcacfce576fb5bf80e01a061a1b347def1
Eishamehmood031 <sp25-bcs-031@cuilahore.edu.pk>
2025-09-10 15:37:23 +0500

    Add files via upload

--- Student.java
+++ Student.java
@@ -3,12 +3,43 @@
 
 
 public class Student{
-  private String Name;
+  private String name;
   private String Email;
   private double cgpa;
 
+public Student(String name,String Email,double cgpa,){
+this.name=name;
+this.email=email;
+this.cgpa=cgpa;
+
+public String getname(){
+return name;
+}
+public String getemail(){
+return email;
+}
+public double getcgpa(){
+return cgpa;
+}
+
+
+public void setname(String name){
+this.name=name
+}
+
+public void setemail(String email){
+this.email=email
+}
+
+public void setcgpa(double cgpa){
+this.cgpa=cgpa
+}
+
+
 public void display(){
-System.out.println("hello student")
+System.out.println("Student name"+name);
+System.out.println("Student email"+email);
+System.out.println("Student cgpa"+cgpa);
 }
 }
 

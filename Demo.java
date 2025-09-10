5a8075c51d19ed2a8a4e6e8735519a43b375976f
Eishamehmood031 <sp25-bcs-031@cuilahore.edu.pk>
2025-09-10 15:28:08 +0500

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+public static void main(String[] args){
+
+Student s1 =new Student;
+s1.display();
+}
+}
+
---
+++ Student.java
@@ -0,0 +1,14 @@
+//Eisha mehmood
+//Sp25-bcs-031
+
+
+public class Student{
+  private String Name;
+  private String Email;
+  private double cgpa;
+
+public void display(){
+System.out.println("hello student")
+}
+}
+
